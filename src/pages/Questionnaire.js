aa5918c81ab3026b2d6ac48c4ddb25b2ce0d2420
mauro125 <43193006+mauro125@users.noreply.github.com>
2021-10-18 20:08:52 -0400

    Revert "Dig 19"

--- src/pages/Questionnaire.js
+++ src/pages/Questionnaire.js
@@ -10,8 +10,6 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 import "firebase/database";
 
-let shuffled = false;
-let shuffledArr=[]
 const Questionnaire = () => {
   const [index, setIndex] = useState(0);
   const [score, setScore] = useState({});
@@ -72,27 +70,7 @@ const Questionnaire = () => {
     }
   };
 
-  let uniqueQuestions = [...questions.reduce((question, obj) => question.set(obj.ques, obj),
-      new Map()).values()]
-
-
-  const shuffle = (array) =>{
-    let i = array.length - 1;
-    for (; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-      shuffled = true
-    }
-    return array;
-  }
-
-  if (!shuffled) {
-    shuffledArr = shuffle(uniqueQuestions)
-  }
-
-  const htmlOfItems = shuffledArr.slice(0, 8).map((question, i) => {
+  const htmlOfItems = questions.slice(0, 8).map((question, i) => {
     return (
         <Carousel.Item key={i}>
           <h2 className='question-title'>{question.ques}</h2>
