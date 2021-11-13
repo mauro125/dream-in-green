fd02d51d98276dead9d801f974baf922cd65b53c
Alex Borrero <aborr033@fiu.edu>
2021-11-13 17:55:22 -0500

    Moved partnership info to Abous us. Imported in same way was made in Home page so that it could be edited in future correctly.

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,8 +3,22 @@ import React from 'react';
 
 const AboutUs = () => {
     return (
-        <div>
-            
+        <div className='container'>
+          <div className='row mt-5 hero-row-reverse-mobile home-row jumbotron'>
+            <div className='col-lg-6 text-center txt-md-left'>
+              <div className='row mb-3'>
+                <h1 className='display-4 font-weight-bold'>
+                  Microsoft and SparkDev
+                </h1>
+              </div>
+              <p className='lead'>
+                Dream in Green teamed up with SparkDev and Microsoft to build this
+                easy to use application so that kids from all over the world can
+                learn about their impact on the environment and how to create a
+                better world.
+              </p>
+            </div>
+          </div>
         </div>
     );
 };
